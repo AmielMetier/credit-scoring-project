6021e71782121278c1cd4bd7fcdc2341c6de66c8
Amiel <amielmetier@gmail.com>
2026-04-06 17:44:34 +0200

    update web page

--- app/app.py
+++ app/app.py
@@ -2,28 +2,28 @@ import streamlit as st
 import requests
 
 # 1. Configuration de la page (Titre de l'onglet, icône, etc.)
-st.set_page_config(page_title="Dashboard Credit Scoring", page_icon="🏦", layout="centered")
+st.set_page_config(page_title="Dashboard Credit Scoring")
 
 # 2. Titre principal sur la page web
-st.title("🏦 Outil d'Évaluation de Risque de Crédit")
-st.write("Ce tableau de bord permet d'interroger notre modèle d'Intelligence Artificielle en temps réel pour aider les conseillers bancaires dans leurs décisions.")
+st.title("Outil d'évaluation de risque de crédit")
+st.write("Ce tableau de bord permet d'interroger notre modèle d'IA pour aider les conseillers bancaires dans leurs décisions.")
 st.divider() # Une petite ligne de séparation esthétique
 
 # 3. Les données de notre client test (Les mêmes que sur Swagger)
 donnees_client_test = {
-  "features": [396899.0, 1.0, 157500.0, 770292.0, 30676.5, 688500.0, 0.010147, -13506.0, -105.0, -2876.0, -4402.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 3.0, 2.0, 2.0, 17.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5059979305057544, 0.5943267272127163, 0.4276573700350293, 0.0876, 0.0763, 0.9816, 0.0, 0.1379, 0.1667, 0.0481, 0.0745, 0.0036, 0.084, 0.0746, 0.9816, 0.0, 0.1379, 0.1667, 0.0458, 0.0731, 0.0011, 0.0864, 0.0758, 0.9816, 0.0, 0.1379, 0.1667, 0.0487, 0.0749, 0.0031, 0.0688, 1.0, 0.0, 1.0, 0.0, -428.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+  "features": [396899.0, 1.0, 157500.0, 770292.0, 30676.5, 688500.0, 0.010147, -13506.0, -105.0, -2876.0, -4402.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 3.0, 2.0, 2.0, 17.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5059979305057544, 0.5943267272127163, 0.4276573700350293, 0.0876, 0.0763, 0.9816, 0.0, 0.1379, 0.1667, 0.0481, 0.0745, 0.0036, 0.084, 0.0746, 0.9816, 0.0, 0.1379, 0.1667, 0.0458, 0.0731, 0.0011, 0.0864, 0.0758, 0.9816, 0.0, 0.1379, 0.1667, 0.0487, 0.0749, 0.0031, 0.0688, 1.0, 0.0, 1.0, 0.0, -428.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0]
 }
 
 st.subheader("Dossier en cours : Client N° 396899")
 
 # 4. Le bouton d'action
-if st.button("Lancer l'analyse de risque 🤖", use_container_width=True):
+if st.button("Lancer l'analyse", use_container_width=True):
     
     # URL de ton API locale
     url_api = "http://127.0.0.1:8000/predict"
     
     # Petit effet visuel d'attente
-    with st.spinner("L'Intelligence Artificielle analyse le dossier..."):
+    with st.spinner("Analyse du dossier en cours..."):
         
         try:
             # Le Dashboard passe un coup de fil à l'API !
@@ -33,20 +33,20 @@ if st.button("Lancer l'analyse de risque 🤖", use_container_width=True):
                 resultat = reponse.json()
                 
                 decision = resultat["decision"]
-                probabilite = resultat["probabilite_defaut"] * 100 # Conversion en %
+                probabilite = resultat["probabilite_defaut"] * 100 
                 
                 st.write("---")
                 # Affichage en Vert ou Rouge selon la décision
                 if decision == "Accepté":
-                    st.success(f"Verdict : Prêt {decision} ✅")
+                    st.success(f"Verdict : Prêt {decision} ")
                 else:
-                    st.error(f"Verdict : Prêt {decision} ❌")
+                    st.error(f"Verdict : Prêt {decision}")
                 
                 # Une belle "métrique" pour afficher le pourcentage
                 st.metric(label="Probabilité de défaut de paiement", value=f"{probabilite:.1f} %")
                 
             else:
-                st.error(f"Erreur de l'API. Code : {reponse.status_code}")
+                st.error(f"Erreur de l'API : {reponse.text}")
                 
         except requests.exceptions.ConnectionError:
             st.error("Impossible de joindre l'API. Vérifie que Uvicorn tourne bien dans ton autre terminal !")
